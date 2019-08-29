3585706991db0139bf0f451e8c44ad93e99eea87
Mihalache Mihai <mihalachemihai30@gmail.com>
2019-08-29 11:53:56 +0200

    First commit with definitions

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    private enum operations {
+        SUB,INS,DEL
+    }
+
+
+
+    private int[][] table;
+    private operations[][] paths;
+
+    public void calculateTable(boolean maximizing, String seq1,String seq2,int[][]score,int gap){}
+    public int getValue(){
+        return 0;
+    }
+}
