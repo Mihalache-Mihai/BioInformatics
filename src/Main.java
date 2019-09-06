c5d26efc683af9eca7e5faf5182ab8d99c82c30b
niklas <nheidemann@techfak.uni-bielefeld.de>
2019-09-06 20:16:01 +0200

    Except for backtracking for affine cost everythings work now

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,35 @@ public class Main {
 
     private static final boolean MAXIMIZING = false;
 
-    public static void main(String[] args) throws FileNotFoundException {
-        new Main(); // wtf ?
+    public static void main(String[] args) throws IOException {
+        new Main();
 
     }
 
-    public Main() throws FileNotFoundException {
-        System.out.println("Welcome. Please type in the name of a file with the score matrix and gapcost");
-        //TODO readFilename
-        String scoreFilename = "score.txt";
+    public Main() throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome. Please type in the name of a file with the score matrix and gapcost (e.g. score.txt)");
+
+        String scoreFilename = "score.txt";//console.readLine();
         readPhyLip(scoreFilename);
 
-        System.out.println("Please specify the gap cost: One variable for linear gapcost, two (opening and continuing) values for affine gapcost");
-        //TODO
-        gapCost = k -> k=='n'?2:1;
+        System.out.println("Please specify the gap cost: One variable for linear gapcost, two (opening and continuing) values for affine gapcost (e.g. 1 or 2 -1 where 2 is the constant and -1 the linear coefficient");
+        String nmbs[]=console.readLine().split(" ");
+        if(nmbs.length==1){
+            gapCost= k->Integer.parseInt(nmbs[0]);
+        }
+        else{
+            gapCost= k->k=='n'?Integer.parseInt(nmbs[0])+Integer.parseInt(nmbs[1]):Integer.parseInt(nmbs[1]);
+        }
 
-        System.out.println("Please type in the name of the file with sequence 1");
-        //TODO
-        String filename1 = "seq1.fasta";
+        System.out.println("Please type in the name of the file with sequence 1 (fasta format)");
+        String filename1 = "seq1.fasta";//console.readLine();
         StringBuilder seq1NameB = new StringBuilder();
         String seq1 = readFasta(filename1, seq1NameB);
         String seq1Name = seq1NameB.toString();
 
-        System.out.println("Please type in the name of the file with sequence 2");
-        //TODO
-        String filename2 = "seq2.fasta";
+        System.out.println("Please type in the name of the file with sequence 2 (fasta format)");
+        String filename2 ="seq2.fasta";// console.readLine();
         StringBuilder seq2NameB = new StringBuilder();
         String seq2 = readFasta(filename2, seq2NameB);
         String seq2Name = seq2NameB.toString();
@@ -42,6 +46,9 @@ public class Main {
         System.out.println(alignments[0]);
         System.out.println(">" + seq2Name + "\n");
         System.out.println(alignments[1]);
+
+        writeAlignment("solution.fastalignment",alignments[0],alignments[1],seq1Name,seq2Name);
+        System.out.println("The alignment was written to solution.fastalignment");
     }
 
     private enum Operation {
@@ -71,45 +78,49 @@ public class Main {
         int[][] D = new int[seq2.length() + 1][seq1.length() + 1];
         int[][] I = new int[seq2.length() + 1][seq1.length() + 1];
         paths = new Operation[seq2.length() + 1][seq1.length() + 1];
-        for (int row = 0; row < S.length; row++) {
-            for (int col = 0; col < S[0].length; col++) {
-                if (row == 0 && col == 0) {
-                    S[0][0] = 0;
-                } else {
-                    if (row > 0) {
-                        D[row][col] = S[row - 1][col] + nGap;
-                        if (row > 1) {
-                            D[row][col] = Math.min(D[row][col], D[row - 1][col] + cGap);
-                        }
-                    }
-                    if (col > 0) {
-                        I[row][col] = S[row][col - 1] + nGap;
-                        if (col > 1) {
-                            I[row][col] = Math.min(I[row][col], I[row][col - 1] + cGap);
-                        }
-                    }
-                    if (row == 0) {
-                        S[row][col] = I[row][col];
-                        paths[row][col]=Operation.DEL;
-                    } else if (col == 0) {
-                        S[row][col] = D[row][col];
-                        paths[row][col]=Operation.INS;
+        try {
+            for (int row = 0; row < S.length; row++) {
+                for (int col = 0; col < S[0].length; col++) {
+                    if (row == 0 && col == 0) {
+                        S[0][0] = 0;
                     } else {
-                        int s=scoreMatrix[alphabet.indexOf(seq2.charAt(row - 1))][alphabet.indexOf(seq1.charAt(col - 1))]+S[row-1][col-1];
-                        if(s<D[row][col]&&s<I[row][col]){
-                            paths[row][col]=Operation.SUB;
-                            S[row][col]=s;
-                        }else if(D[row][col]<I[row][col]){
-                            paths[row][col]=Operation.INS;
-                            S[row][col]=D[row][col];
+                        if (row > 0) {
+                            D[row][col] = S[row - 1][col] + nGap;
+                            if (row > 1) {
+                                D[row][col] = Math.min(D[row][col], D[row - 1][col] + cGap);
+                            }
                         }
-                        else{
-                            paths[row][col]=Operation.DEL;
-                            S[row][col]=I[row][col];
+                        if (col > 0) {
+                            I[row][col] = S[row][col - 1] + nGap;
+                            if (col > 1) {
+                                I[row][col] = Math.min(I[row][col], I[row][col - 1] + cGap);
+                            }
+                        }
+                        if (row == 0) {
+                            S[row][col] = I[row][col];
+                            paths[row][col] = Operation.DEL;
+                        } else if (col == 0) {
+                            S[row][col] = D[row][col];
+                            paths[row][col] = Operation.INS;
+                        } else {
+                            int s = scoreMatrix[alphabet.indexOf(seq2.charAt(row - 1))][alphabet.indexOf(seq1.charAt(col - 1))] + S[row - 1][col - 1];
+                            if (s < D[row][col] && s < I[row][col]) {
+                                paths[row][col] = Operation.SUB;
+                                S[row][col] = s;
+                            } else if (D[row][col] < I[row][col]) {
+                                paths[row][col] = Operation.INS;
+                                S[row][col] = D[row][col];
+                            } else {
+                                paths[row][col] = Operation.DEL;
+                                S[row][col] = I[row][col];
+                            }
                         }
                     }
                 }
             }
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid character, be aware that the score matrix has to contain the characters and is case sensitive");
+            System.exit(1);
         }
         return S;
     }
@@ -267,8 +278,13 @@ public class Main {
                 this.alphabet.add(matrixLines.get(i).charAt(0));
                 line = matrixLines.get(i).substring(2);
                 String[] nmbs = line.split(" ");
-                for (int j = 0; j < matrixLines.size(); j++) {
-                    this.score[i][j] = Integer.parseInt(nmbs[j]);
+                int l=0;
+                for (int j = 0; j < nmbs.length; j++) {
+                    if(nmbs[j].equals("")) {
+                        l++;
+                        continue;
+                    }
+                    this.score[i][j-l] = Integer.parseInt(nmbs[j]);
                 }
             }
 
@@ -276,6 +292,21 @@ public class Main {
             System.out.println("File " + filename + " not found!");
         } catch (Exception e) {
             System.out.println("Invalid format!");
+        }
+
+    }
+
+    private void writeAlignment(String filename, String seq1,String seq2,String name1,String name2){
+        try {
+            PrintWriter writer=new PrintWriter(new File(filename));
+            writer.println(">"+name1);
+            writer.println(seq1);
+            writer.println();
+            writer.println(">"+name2);
+            writer.println(seq2);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
 
     }
