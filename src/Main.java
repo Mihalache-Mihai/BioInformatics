fb661bcf67e9efeeea8ea83c5a10e85101eb623c
niklas <nheidemann@techfak.uni-bielefeld.de>
2019-09-07 16:35:49 +0200

    everything done now

--- src/Main.java
+++ src/Main.java
@@ -18,12 +18,11 @@ public class Main {
         readPhyLip(scoreFilename);
 
         System.out.println("Please specify the gap cost: One variable for linear gapcost, two (opening and continuing) values for affine gapcost (e.g. 1 or 2 -1 where 2 is the constant and -1 the linear coefficient");
-        String nmbs[]=console.readLine().split(" ");
-        if(nmbs.length==1){
-            gapCost= k->Integer.parseInt(nmbs[0]);
-        }
-        else{
-            gapCost= k->k=='n'?Integer.parseInt(nmbs[0])+Integer.parseInt(nmbs[1]):Integer.parseInt(nmbs[1]);
+        String nmbs[] = console.readLine().split(" ");
+        if (nmbs.length == 1) {
+            gapCost = k -> Integer.parseInt(nmbs[0]);
+        } else {
+            gapCost = k -> k == 'n' ? Integer.parseInt(nmbs[0]) + Integer.parseInt(nmbs[1]) : Integer.parseInt(nmbs[1]);
         }
 
         System.out.println("Please type in the name of the file with sequence 1 (fasta format)");
@@ -33,23 +32,36 @@ public class Main {
         String seq1Name = seq1NameB.toString();
 
         System.out.println("Please type in the name of the file with sequence 2 (fasta format)");
-        String filename2 ="seq2.fasta";// console.readLine();
+        String filename2 = "seq2.fasta";// console.readLine();
         StringBuilder seq2NameB = new StringBuilder();
         String seq2 = readFasta(filename2, seq2NameB);
         String seq2Name = seq2NameB.toString();
 
-        int[][] table = affine_table(MAXIMIZING, seq1, seq2, score, gapCost);
-        printMatrix(table);
-        System.out.println("The cost of an optimal alignment is " + getLastValueFromMatrix(table));
-        String[] alignments = getAlignment(seq1, seq2, table);
-        System.out.println(">" + seq1Name + "\n");
-        System.out.println(alignments[0]);
-        System.out.println(">" + seq2Name + "\n");
-        System.out.println(alignments[1]);
+        int[][] table;
+        String[] alignments;
+        try {
+            if (nmbs.length == 1) {
+                table = linear_table(MAXIMIZING, seq1, seq2, score, gapCost);
+                alignments = getAlignment(seq1, seq2, table);
+            } else {
+                table = affine_table(MAXIMIZING, seq1, seq2, score, gapCost);
+                alignments = getAffineAlignment(seq1, seq2, table);
+            }
+            printMatrix(table);
+            System.out.println("The cost of an optimal alignment is " + getLastValueFromMatrix(table));
+            System.out.println(">" + seq1Name + "\n");
+            System.out.println(alignments[0]);
+            System.out.println(">" + seq2Name + "\n");
+            System.out.println(alignments[1]);
 
-        writeAlignment("solution.fastalignment",alignments[0],alignments[1],seq1Name,seq2Name);
-        System.out.println("The alignment was written to solution.fastalignment");
-    }
+            writeAlignment("solution.fastalignment", alignments[0], alignments[1], seq1Name, seq2Name);
+            System.out.println("The alignment was written to solution.fastalignment");
+
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Error: You probably used a character in your sequence, that you didnt specified in the score matrix (case sensitive)");
+            System.exit(1);
+        }
+ }
 
     private enum Operation {
         SUB, INS, DEL
@@ -62,9 +74,59 @@ public class Main {
 
     private static Operation[][] paths;
 
+    public String[] getAffineAlignment(String seq1, String seq2, int[][] resultTable) {
+        int nGap = gapCost.calc('n');//new block
+        int cGap = gapCost.calc('c');//continue
+        int i = seq2.length();
+        int j = seq1.length();
+        StringBuilder result1 = new StringBuilder();
+        StringBuilder result2 = new StringBuilder();
+        boolean mustD=false;
+        boolean mustI=false;
+        while (i > 0 || j > 0) {
+            int cur = resultTable[i][j];
+            if (!mustI&&(mustD||i > 0 && resultTable[i - 1][j] + nGap == cur)) {
+                mustD=false;
+                result1.insert(0, seq2.charAt(i - 1));
+                result2.insert(0, '_');
+                i--;
+            }
+            else if (mustI||j > 0 && resultTable[i][j - 1] + nGap == cur ) {
+                mustI=false;
+                result1.insert(0, '_');
+                result2.insert(0, seq1.charAt(j - 1));
+                j--;
+            } else if(i > 1 && D[i - 1][j] + cGap == cur) {
+                result1.insert(0, seq2.charAt(i - 1));
+                result2.insert(0, '_');
+                resultTable[i - 1][j] = D[i - 1][j];
+                i--;
+                mustD=true;
+            }else if ( j > 1 && I[i][j - 1] + cGap == cur){
+                result1.insert(0, '_');
+                result2.insert(0, seq1.charAt(j - 1));
+                resultTable[i][j - 1] = I[i][j - 1];
+                j--;
+                mustI=true;
+            }
+            else if (i > 0 && j > 0 && resultTable[i - 1][j - 1] + score[alphabet.indexOf(seq2.charAt(i - 1))][alphabet.indexOf(seq1.charAt(j - 1))] == cur) {
+                result1.insert(0, seq2.charAt(i - 1));
+                result2.insert(0, seq1.charAt(j - 1));
+                i--;
+                j--;
+            }
+            System.out.println(i+ "" +j);
+        }
+        return new String[]{result2.toString(), result1.toString()};
+    }
+
+    private int[][] D;
+    private int[][] I;
+
     /**
      * The version with affine cost function
-     * @param maximizing will always minimize
+     *
+     * @param maximizing  will always minimize
      * @param seq1
      * @param seq2
      * @param scoreMatrix
@@ -75,8 +137,8 @@ public class Main {
         int nGap = gapCost.calc('n');//new block
         int cGap = gapCost.calc('c');//continue
         int[][] S = new int[seq2.length() + 1][seq1.length() + 1];
-        int[][] D = new int[seq2.length() + 1][seq1.length() + 1];
-        int[][] I = new int[seq2.length() + 1][seq1.length() + 1];
+        D = new int[seq2.length() + 1][seq1.length() + 1];
+        I = new int[seq2.length() + 1][seq1.length() + 1];
         paths = new Operation[seq2.length() + 1][seq1.length() + 1];
         try {
             for (int row = 0; row < S.length; row++) {
@@ -118,7 +180,7 @@ public class Main {
                     }
                 }
             }
-        }catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid character, be aware that the score matrix has to contain the characters and is case sensitive");
             System.exit(1);
         }
@@ -188,6 +250,7 @@ public class Main {
         return resultTable.length > 0 ? resultTable[resultTable.length - 1][resultTable[0].length - 1] : 0;
     }
 
+
     public String[] getAlignment(String seq1, String seq2, int[][] resultTable) {
         int x = resultTable.length - 1;
         int y = resultTable[0].length - 1;
@@ -209,7 +272,7 @@ public class Main {
                 x--;
             }
         }
-        return new String[]{result1.toString(), result2.toString()};
+        return new String[]{result2.toString(), result1.toString()};
     }
 
 
@@ -222,7 +285,9 @@ public class Main {
      */
 
     private String eliminateWhiteSpace(String s) {
-        if(s==null){return null;}
+        if (s == null) {
+            return null;
+        }
         return s.replaceAll(" ", "").replaceAll("\t", "").replaceAll("\n", "");
     }
 
@@ -278,13 +343,13 @@ public class Main {
                 this.alphabet.add(matrixLines.get(i).charAt(0));
                 line = matrixLines.get(i).substring(2);
                 String[] nmbs = line.split(" ");
-                int l=0;
+                int l = 0;
                 for (int j = 0; j < nmbs.length; j++) {
-                    if(nmbs[j].equals("")) {
+                    if (nmbs[j].equals("")) {
                         l++;
                         continue;
                     }
-                    this.score[i][j-l] = Integer.parseInt(nmbs[j]);
+                    this.score[i][j - l] = Integer.parseInt(nmbs[j]);
                 }
             }
 
@@ -296,13 +361,13 @@ public class Main {
 
     }
 
-    private void writeAlignment(String filename, String seq1,String seq2,String name1,String name2){
+    private void writeAlignment(String filename, String seq1, String seq2, String name1, String name2) {
         try {
-            PrintWriter writer=new PrintWriter(new File(filename));
-            writer.println(">"+name1);
+            PrintWriter writer = new PrintWriter(new File(filename));
+            writer.println(">" + name1);
             writer.println(seq1);
             writer.println();
-            writer.println(">"+name2);
+            writer.println(">" + name2);
             writer.println(seq2);
             writer.close();
         } catch (FileNotFoundException e) {
